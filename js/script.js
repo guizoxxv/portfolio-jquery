a1a15609caf748f6f35fa60debdf097f336b036a
guizo <guizoxxv@gmail.com>
2017-10-02 15:03:50 -0300

    Single script for all modals.

--- js/script.js
+++ js/script.js
@@ -1,60 +1,14 @@
 $(document).ready(function() {
-
-  $('#card-1').on('click', function() {
-    $('#modal-1').bPopup({
+  $(document).on('click', '.card', function() {
+    $(this).next('.modal').bPopup({
       modalClose: true,
+      appending: false, // Keep modal element DOM position instead of appending to body (default)
       speed: 500,
       amsl: 0, // center modal on page
       closeClass: 'close'
     });
   });
-
-  $('#card-2').on('click', function() {
-    $('#modal-2').bPopup({
-      modalClose: true,
-      speed: 500,
-      amsl: 0, // center modal on page
-      closeClass: 'close'
-    });
-  });
-
-  $('#card-3').on('click', function() {
-    $('#modal-3').bPopup({
-      modalClose: true,
-      speed: 500,
-      amsl: 0, // center modal on page
-      closeClass: 'close'
-    });
-  });
-
-  $('#card-4').on('click', function() {
-    $('#modal-4').bPopup({
-      modalClose: true,
-      speed: 500,
-      amsl: 0, // center modal on page
-      closeClass: 'close'
-    });
-  });
-
-  $('#card-5').on('click', function() {
-    $('#modal-5').bPopup({
-      modalClose: true,
-      speed: 500,
-      amsl: 0, // center modal on page
-      closeClass: 'close'
-    });
-  });
-
-  $('#card-6').on('click', function() {
-    $('#modal-6').bPopup({
-      modalClose: true,
-      speed: 500,
-      amsl: 0, // center modal on page
-      closeClass: 'close'
-    });
-  });
-
+  
   // Prevent parent element from scrolling when scroll reachs top or bottom of modal
   $('.modal').scrollLock();
-
 });
